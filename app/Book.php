a4a45cdd9b48d2a17b2a670b8cad21d40103d274
lukelliotfrost <luke@radio.co>
2020-07-28 19:57:06 +0100

    Added mass assignment and relationships to the models.

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+
+    protected $fillable = ['user_id', 'title', 'description'];
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
